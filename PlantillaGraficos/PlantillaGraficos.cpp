9b235cf301af7a5c6c3740313a9ba95dfb5dedfe
alumno <alumno@E329>
2020-01-17 10:52:29 -0700

    ventana y color

--- PlantillaGraficos/PlantillaGraficos.cpp
+++ PlantillaGraficos/PlantillaGraficos.cpp
@@ -2,18 +2,61 @@
 //
 
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define GLEW_STATIC
+
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   //Declarar una vetana
+	GLFWwindow* window;
+	
+	//si no se pudo inicializar glfw terminamos la ejecución
+
+	if (!glfwInit()) {
+		exit(EXIT_FAILURE);
+	}
+
+	//Si se pudo inicial glfw, se inicia la ventana
+
+	window = glfwCreateWindow(800,600, "ventana",NULL, NULL);
+
+	// si no se pudo crear la ventana, terminamos la ejecución
+
+	if (!window) {
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	//Establecemos la ventana como contexto
+	glfwMakeContextCurrent(window);
+
+	//ciclo de dibujo (Draw loop)
+
+	while (!glfwWindowShouldClose(window))
+	{
+		//establecer la región de dibujo
+		glViewport(0, 0, 800, 600);
+		//establecemos el color de borrado, valores rgba, van de 0 a 1
+		glClearColor(.3,.21,.6,1);
+		//Borrar!
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
+		//Actualizar valores y dibujar
+
+		glfwSwapBuffers(window);
+	}
+
+	//Despues del ciclo de dibujo eliminamos ventana y procesos de glfw
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
